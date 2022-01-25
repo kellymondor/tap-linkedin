5e9b59797402046b5b977a652e54a29bdd21dd97
kellymondor <kellymondor@gmail.com>
2022-01-25 16:29:19 -0700

    Update base_stream.py

--- tap_linkedin/streams/base_stream.py
+++ tap_linkedin/streams/base_stream.py
@@ -21,9 +21,9 @@ class BaseStream():
         
         schema_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "schemas")
         schema_json = singer.utils.load_json(f"{schema_path}/{self.stream_id}.json")
-        schema = Schema(schema_json)
+        # schema = schema_json
         
-        return schema
+        return schema_json
     
     def write_record(self, record, time_extracted):  
         
